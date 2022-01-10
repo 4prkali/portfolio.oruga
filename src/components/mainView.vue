cc27e82d864f50c33248ac776a7749b5b9aedb41
Alberto Rosello Martin <55388819+AprKali@users.noreply.github.com>
2022-01-10 14:44:18 +0100

    added icons and main text

--- src/components/mainView.vue
+++ src/components/mainView.vue
@@ -3,22 +3,31 @@
     <div class="hero-body">
       <div class="container">
         <div class="columns">
-          <div class="column is-half my-auto">
+          <div class="column is-half">
             <div class="title is-1 has-text-black ">
               Hi,<br>
-              I'm Alberto,<br>
-              junior developer.
+              I'm <span style="color: #FF993B">Alberto</span>,<br>junior developer.
             </div>
-            <o-button class="mt-3">Contact me</o-button>
+            <div class="subtitle mt-1 has-text-justified has-text-grey">
+              Passionate junior developer with knowledge in multiple languages as Java, HTML
+              and SQL. Seeking to start working and learn every day new ways to improve as a
+              programmer.
+            </div>
+            <o-button class="mt-3">CONTACT ME</o-button>
           </div>
-          <div class="column is-half ">
-            <lottie-player class="animation" src="https://assets8.lottiefiles.com/packages/lf20_jlcrxaq6.json"
-                           background="transparent"
-                           speed="1"
-                           loop autoplay>
-            </lottie-player>
+          <div class="column is-half has-text-right my-auto">
+            <div class="columns">
+              <div class="column ">
+                <o-icon pack="fas" icon="fa-brands fa-instagram"></o-icon>
+              </div>
+            </div>
+            <div class="columns">
+              <div class="column">a</div>
+            </div>
+            <div class="columns">
+              <div class="column">a</div>
+            </div>
           </div>
-
         </div>
       </div>
     </div>
@@ -32,6 +41,6 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import{ createApp }from'vue'
 import App from'./App.vue'
-
 import Oruga from'@oruga-ui/oruga-next'
 import{ bulmaConfig }from'@oruga-ui/theme-bulma'
 
 import'@/assets/styles/style.scss'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App)
-    .use(Oruga, bulmaConfig)
-    .mount('#app')
+/* add icons to the library */
+library.add(fas)
+library.add(faFontAwesome)
+
+createApp(App).use(Oruga, bulmaConfig,{ iconPack: 'fas',}).mount('#app')
