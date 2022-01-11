727a8f7482fb2715d4d4ea3145b612fa1aa344e3
AprKali <albertoroosellomartin@gmail.com>
2022-01-11 01:24:24 +0100

    first navbar iteration
    fix buttons on top change border and add shadow on hover

--- src/components/mainView.vue
+++ src/components/mainView.vue
@@ -4,11 +4,11 @@
       <div class="container">
         <div class="columns">
           <div class="column is-half">
-            <div class="title is-1 has-text-black ">
+            <div class="title has-text-black ">
               Hi,<br>
               I'm <span style="color: #FF993B">Alberto</span>,<br>junior developer.
             </div>
-            <div class="subtitle mt-1 has-text-justified has-text-grey">
+            <div class=" subtitle mt-1 has-text-justified has-text-grey">
               Passionate junior developer with knowledge in multiple languages as Java, HTML
               and SQL. Seeking to start working and learn every day new ways to improve as a
               programmer.
@@ -16,17 +16,6 @@
             <o-button class="mt-3">CONTACT ME</o-button>
           </div>
           <div class="column is-half has-text-right my-auto">
-            <div class="columns">
-              <div class="column ">
-                <o-icon pack="fas" icon="fa-brands fa-instagram"></o-icon>
-              </div>
-            </div>
-            <div class="columns">
-              <div class="column">a</div>
-            </div>
-            <div class="columns">
-              <div class="column">a</div>
-            </div>
           </div>
         </div>
       </div>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,15 +2,26 @@
   <nav class="navbar is-white has-shadow is-fixed-top " role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-        </a>
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <o-button>Home</o-button>
+          </div>
+          <div class="navbar-item">
+            <o-button>Projects</o-button>
 
+          </div>
+          <div class="navbar-item">
+            <o-button>Tools</o-button>
+          </div>
+        </div>
+      </div>
     </div>
 
   </nav>
