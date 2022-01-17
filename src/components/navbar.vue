1c66e5fa0d1d3aab824a15858dcf22efed1da4be
Alberto Rosello Martin <55388819+AprKali@users.noreply.github.com>
2022-01-17 14:47:56 +0100

    added navbar brand and personal logo

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,6 +2,10 @@
   <nav class="navbar is-white has-shadow is-fixed-top " role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
+        <div class="navbar-item">
+          <img :src="require('@/assets/logoARM.png')">
+          <div class="has-text-black ml-4">Alberto Roselló Martín</div>
+        </div>
         <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -9,6 +13,9 @@
         </a>
       </div>
       <div class="navbar-menu">
+        <div class="navbar-start">
+
+        </div>
         <div class="navbar-end">
           <div class="navbar-item">
             <a class="has-text-black" href="#">Home</a>
