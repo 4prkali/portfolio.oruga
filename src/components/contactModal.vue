af2c3550cd16c9be6c0df6eb06ed463492f04e7c
AprKali <4prkali@gmail.com>
2022-01-19 18:28:22 +0100

    added working mail modal missing control errors

---
+++ src/components/contactModal.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card">
+    <form class="card-content" ref="form" @submit.prevent="sendEmail">
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input name="user_name" class="input" type="text" placeholder="Text input">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Subject</label>
+        <div class="control">
+          <input name="subject" class="input" type="text" placeholder="Subject input">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control has-icons-left has-icons-right">
+          <input class="input"  name="user_email" type="email" placeholder="Email input" value="">
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+        </div>
+      </div>
+
+
+
+      <div class="field">
+        <label class="label">Message</label>
+        <div class="control">
+          <textarea name="message" class="textarea" placeholder="Textarea"></textarea>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column">
+          <input type="submit"  class="button" value="Send email" />
+        </div>
+      </div>
+    </form>
+
+  </div>
+
+</template>
+
+<script>
+import emailjs from '@emailjs/browser';
+
+export default {
+  methods: {
+    sendEmail() {
+      emailjs.sendForm('service_dycrr2y', 'template_y6ncltx', this.$refs.form, 'user_rouNg7WFBELx4sABhSp2i')
+          .then((result) => {
+            console.log('SUCCESS!', result.text);
+          }, (error) => {
+            console.log('FAILED...', error.text);
+          });
+    }
+  }
+}
+</script>
--- src/components/mainView.vue
+++ src/components/mainView.vue
@@ -7,12 +7,12 @@
             <div class="is-size-2 is-size-4-mobile has-text-black">
               Junior Developer <span class="has-text-info">creating</span> and <span class="has-text-info">designing</span> solutions for your projects.
             </div>
-            <div class=" is-size-5 is-size-6-mobile mt-5 has-text-justified has-text-grey">
+            <div class="is-size-5 is-size-6-mobile mt-5 has-text-justified has-text-grey">
               I'm Alberto Roselló, a passionate junior developer based on Spain with knowledge in multiple languages as Java, HTML, SQL, Kotlin and Swift.
               Seeking to start working and learn every day new ways to improve as a programmer.
             </div>
             <div class="has-text-centered-mobile">
-              <o-button class="mt-5 mr-2">Email me</o-button>
+              <o-button @click="isCardModalActive = true" class="mt-5 mr-2">Email me</o-button>
               <a v-on:click="github" class="button is-white mt-5">
                 <img :src="require('@/assets/github.png')" style="width:32px" alt="lg_github">
               </a>
@@ -25,11 +25,17 @@
       </div>
     </div>
   </section>
+
+  <o-modal animation="zoom-out" can-cancel="['escape', 'x', 'outside', 'button']" v-model:active="isCardModalActive">
+    <contact-modal></contact-modal>
+  </o-modal>
+  
 </template>
 
 <script>
 // eslint-disable-next-line no-unused-vars
 
+import ContactModal from "./contactModal";
 export default {
   name: "mainView",
   methods:{
@@ -38,9 +44,14 @@ export default {
     },
     github: function (){
       window.location.href = "https://github.com/4prKali";
+    },
+  },
+  data() {
+    return {
+      isCardModalActive: false
     }
   },
-  components: {}
+  components: {ContactModal}
 }
 
 </script>
